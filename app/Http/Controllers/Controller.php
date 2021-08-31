90f12c4ca09bf5f92dcdaf25fed2af43cecf9cc4
vagrant <vagrant@homestead>
2021-08-31 18:37:59 +0000

    init commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Faker\Factory;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+	protected function getNews(): array
+	{
+		$faker = Factory::create('ru_RU');
+		$data = [];
+		$countNumber = mt_rand(5,15);
+		for($i=0; $i<$countNumber; $i++) {
+			$data[] = [
+				'id' => $i+1,
+				'title' => $faker->jobTitle(),
+				'description' => $faker->sentence(3),
+				'author' => $faker->name(),
+				'created_at' => now()
+			];
+		}
+		return $data;
+	}
+}
