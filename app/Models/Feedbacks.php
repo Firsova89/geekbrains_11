6104dea58b9233ab193b8973e45aae1f1d3feb85
Firsova89 <75429459+Firsova89@users.noreply.github.com>
2021-09-18 20:19:47 +0300

    Добавлены миграции и seed для отзывов.

---
+++ app/Models/Feedbacks.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Feedbacks extends Model
+{
+
+	protected $table = "feedbacks";
+
+	public function getFeedbacks()
+	{
+		return \DB::table($this->table)
+            ->select(["username","userfeedback","created_at"])
+            ->orderBy('created_at', 'desc')
+            ->take(20)
+            ->get();
+	}
+
+	public function addFeedback(string $username, string $feedback)
+	{
+        \DB::table($this->table)->insert([
+            'username' => $username,
+            'userfeedback' => $feedback,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+	}
+}
